314901b2266d538004e076d627296fb869fbcfae
Max Kovalenko <mxss1998@yandex.ru>
2018-10-28 23:20:56 +0300

    basics (tornado / numpy / etc.)

---
+++ modules/site/layout_generator.py
@@ -0,0 +1,10 @@
+class LayoutGenerator():
+    @staticmethod
+    def gen_header():
+        """
+        :return:
+        """
+        return "<center>" \
+               "<h1>Python-math</h1>" \
+               "<h3>My study project, using numpy & stuff</h3>" \
+               "</center>"
--- start.py
+++ start.py
@@ -1,4 +1,15 @@
+import tornado.ioloop
+import tornado.web
+from site.nav import IndexHandler
+
+
+def make_tornado_app():
+    return tornado.web.Application([
+        (r"/", IndexHandler),
+    ])
+
 
 if __name__ == "__main__":
-    print("yep")
-    pass
+    app = make_tornado_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
