3df77111e2590a9edff126b6489637b7e19e37cf
Max Kovalenko <mxss1998@yandex.ru>
2018-11-25 22:33:23 +0300

    converter from radian to degrees

--- modules/geometry/degrees_module.py
+++ modules/geometry/degrees_module.py
@@ -3,5 +3,9 @@ import math
 
 class DegreesModule:
     @staticmethod
-    def degrees_to_radian(degrees: int):
+    def degrees_to_radian(degrees: float):
         return (degrees * math.pi) / 180
+
+    @staticmethod
+    def radian_to_degrees(radian: float):
+        return radian * (180 / math.pi)
--- pages/geometry_1.py
+++ pages/geometry_1.py
@@ -12,9 +12,30 @@ class Geometry1Handler(tornado.web.RequestHandler, ABC):
 
         degrees_to_check = [1, 15, 20, 40, 60, 80, 90, 120, 180, 360]
 
-        content = "<ul>"
+        self.write("<center>")
+        content = "<table border='1' cellpadding='2'>" \
+                  "<tr>" \
+                  "<th>Degrees</th>" \
+                  "<th>Radians</th>" \
+                  "</tr>"
         for degrees in degrees_to_check:
-            content += "<li>{0} degrees to radians: {1}</li>".format(str(degrees),
-                                                                     str(DegreesModule.degrees_to_radian(degrees)))
-        content += "</ul>"
+            content += "<tr><td>{0}</td><td>{1}</td></tr>".format(str(degrees),
+                                                                  str(DegreesModule.degrees_to_radian(degrees)))
+        content += "</table>"
         self.write(content)
+
+        self.write(LayoutGenerator.gen_title("Converter from radian to degrees"))
+
+        radians_list = [0, 0.5, 1, 1.5, 2, 4, 6, 8, 10, 1200, -5]
+
+        content = "<table border='1' cellpadding='2'>" \
+                  "<tr>" \
+                  "<th>Radians</th>" \
+                  "<th>Degrees</th>" \
+                  "</tr>"
+        for radians in radians_list:
+            content += "<tr><td>{0}</td><td>{1}</td></tr>".format(str(radians),
+                                                                      str(DegreesModule.radian_to_degrees(radians)))
+        content += "</table>"
+        self.write(content)
+        self.write("</center>")
