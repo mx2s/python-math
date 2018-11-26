9103a5946478671a399ccbf1a56e6e06785e0641
Max Kovalenko <mxss1998@yandex.ru>
2018-11-27 00:03:28 +0300

    discriminant calculation WIP

--- pages/geometry_2.py
+++ pages/geometry_2.py
@@ -9,7 +9,6 @@ from modules.site.layout_generator import LayoutGenerator
 
 class Geometry2Handler(tornado.web.RequestHandler, ABC):
     def get(self):
-        self.write(str(MiscModule.get_arc_length(90,4)))
         self.write(LayoutGenerator.gen_header())
 
         block = ResultBlock()
@@ -25,3 +24,18 @@ class Geometry2Handler(tornado.web.RequestHandler, ABC):
             block.table_data.append(arr)
 
         self.write(block.render())
+
+        block = ResultBlock()
+        block.title = "Sector area"
+        block.table_columns = ["Angle", "Radius", "Area"]
+
+        for i in range(0, 10):
+            arr = [
+                randint(0, 700) * 0.5,
+                randint(0, 10000) * 0.5
+            ]
+            arr.append(MiscModule.get_sector_area(arr[0], arr[1]))
+            block.table_data.append(arr)
+
+        self.write(block.render())
+
